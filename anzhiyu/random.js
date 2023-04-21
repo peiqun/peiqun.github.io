b53d076581c33ea86c94e3d4c23c93896451ff0b
peiqun <parviz.pei@outlook.com>
2023-04-21 22:07:07 +0800

    Site updated: 2023-04-21 22:07:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/04/19/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/04/21/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
